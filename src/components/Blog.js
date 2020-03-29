5ecdfe453b55a6f3cb3bf1842a0b42bfcedf6d29
Ville <korppi.villekorppi.ville@gmail.com>
2020-03-29 20:27:05 +0300

    adde less more buttons for blog rows

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import ChangeVisibility from './ChangeVisibility'
+
 const Blog = ({ blog }) => (
   <div>
+
+  <div>
     {blog.title} {blog.author}
+  </div>
+
+  <ChangeVisibility ref={React.createRef()}>
+  <div>
+    {blog.url} 
+  </div>
+  <div>
+    {blog.likes} 
+  </div>
+  <div>
+    {blog.user.name} 
+  </div>
+  </ChangeVisibility>
+
   </div>
 )
 
